083c1c194664c3c0a2ff2b8e1f3d018f00b67169
Dawid Dymke <dawid584@yahoo.com>
2022-09-09 10:26:20 +0200

    fix exercise

--- Untitled-1.py
+++ Untitled-1.py
@@ -11,18 +11,20 @@ class BaseContact:
         self.name = name
         self.phone = phone
         self.email = email
-
     #property
+
+
     def label_length(self):
        print("długość imienia:  " + len(self.name))
-    self.label_length()     
+       self.label_length()   
+     
   
 
     def contact(self): 
        print(f'Wybieram numer +48 {self.phone} i dzwonię do {self.name}') 
        print(f'Wizytówka prywatna {self.name} , +48{self.phone} , {self.email}')
+       self.contact() 
     
-    self.contact()
 
 class BusinessContact(BaseContact):
     def __init__(self, name, phone, email, company_position, company_name, company_number):
@@ -30,12 +32,15 @@ class BusinessContact(BaseContact):
        self.company_position = company_position
        self.company_name = company_name
        self.company_number = company_number
-       
+
+  
+
     def contact(self): 
        print(f'Wybieram numer służbowy +48 {self.phone} i dzwonię do {self.company_number}')  
        print(f'Wizytówka służbowa {self.company_position} , +48{self.company_number} , {self.company_name}')
-    self.contact()
+       self.contact()
     
+
 def fake_contacts(is_business, amount):
     fake = Faker(['th_TH', 'pl_PL'])
     company_position = fake.job()
@@ -55,8 +60,8 @@ def fake_contacts(is_business, amount):
         lista_of_contacts.append(card)
 
     return lista_of_contacts
-
-
+    
+    
 
 n = int(input('wpisz '))
 
